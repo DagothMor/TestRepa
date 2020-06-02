673b725ac5630d16c6695c31ed271757abc7eafc
DagothMor <siegfried1997@gmail.com>
2020-06-02 23:37:39 +0300

    I make a Console application on .net core 3.0

---
+++ MyFirstSolution/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Press any key to start");
+			Console.ReadLine();
+			// do something...
+			Console.WriteLine("Press any key to end");
+			Console.ReadLine();
+		}
+	}
+}
